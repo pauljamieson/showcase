622d4b50f297913f077d88e4ca22c20690ef3d71
Paul <paul.jamieson.ca@gmail.com>
2024-09-08 02:45:19 -0500

    update

--- api/src/controllers/migrate/migrate.ts
+++ api/src/controllers/migrate/migrate.ts
@@ -8,7 +8,7 @@ async function POST(req: Request, res: Response) {
   const { filename, size, views, rating, tags, people } = req.body;
 
   try {
-    /*const file = await prisma.videoFile.findFirst({
+    const file = await prisma.videoFile.findFirst({
       where: {
         filename,
         size,
@@ -17,35 +17,7 @@ async function POST(req: Request, res: Response) {
         tags: true,
         people: true,
       },
-    });*/
-
-    const result = await prisma.videoFile.findMany({
-      where: { createdAt: { lte: new Date("2024-09-07 02:00:04.051") } },
     });
-    
-    result.map(async (v: any) => {
-      try {
-        await prisma.videoFile.update({
-          where: {
-            id: v.id,
-          },
-          data: {
-            tags: {
-              connectOrCreate: {
-                where: { name: "new" },
-                create: { name: "new", userId: 1 },
-              },
-            },
-          },
-        });
-      } catch (error: any) {
-        if ((error.code = "P2002")) {
-        } else {
-          console.error(error, "");
-        }
-      }
-    });
-    /*
     if (!file) throw "File not found.";
 
     const tagList = (tags ? tags.split(",") : []).map((v: string) => {
@@ -82,7 +54,7 @@ async function POST(req: Request, res: Response) {
         people: { connectOrCreate: peopleList },
       },
     });
-*/
+
     res.json({ status: "success" });
   } catch (error) {
     console.error(error);
