6871b5a323bd05234bdaaf19cd256fb8768e33f1
Paul <paul.jamieson.ca@gmail.com>
2024-09-14 01:30:01 -0500

    update

--- backup_db.sh
+++ backup_db.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+read -p "(d)ev or (p)rod deployment": deployment
+if ! [[ $deployment =~ [DdPp] ]]; then
+  echo Enter p or d only.
+  exit 1
+fi
 read -p "Enter backup file name: " filename
-read -p "Enter name of showcase database container: " contname
 
-docker compose -f docker-compose-dev.yml stop
-docker run --rm --volumes-from $contname -v $(pwd):/backup ubuntu tar cvf /backup/$filename /var/lib/postgresql/data
-docker compose -f docker-compose-dev.yml start 
+
+if [[ $deployment =~ [Dd] ]]; then
+  docker compose -f docker-compose-dev.yml stop
+  docker run --rm --volumes-from showcase-db-1 -v $(pwd):/backup ubuntu tar cvf /backup/$filename /var/lib/postgresql/data
+  docker compose -f docker-compose-dev.yml start 
+fi
+if [[ $deployment =~ [Pp] ]]; then
+  docker compose stop
+  docker run --rm --volumes-from showcase-db-1 -v $(pwd):/backup ubuntu tar cvf /backup/$filename /var/lib/postgresql/data
+  docker compose start
+fi
--- restore_db.sh
+++ restore_db.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+read -p "(d)ev or (p)rod deployment": deployment
+if ! [[ $deployment =~ [DdPp] ]]; then
+  echo Enter p or d only.
+  exit 1
+fi
 read -p "Enter backup file name: " filename
-read -p "Enter name of volume to restore to: " volname
 
 
-docker compose -f docker-compose-dev.yml stop
-docker run --rm -v $(pwd):/backup -v $volname:/var/lib/postgresql/data ubuntu tar xvf /backup/$filename 
-docker compose -f docker-compose-dev.yml start 
+if [[ $deployment =~ [Dd] ]]; then
+  docker compose -f docker-compose-dev.yml stop
+  docker run --rm -v $(pwd):/backup -v showcase_pgdata:/var/lib/postgresql/data ubuntu tar xvf /backup/$filename 
+  docker compose -f docker-compose-dev.yml start 
+fi
+if [[ $deployment =~ [Pp] ]]; then
+  docker compose stop
+  docker run --rm -v $(pwd):/backup -v showcase_prod_pgdata:/var/lib/postgresql/data ubuntu tar xvf /backup/$filename 
+  docker compose start 
+fi
