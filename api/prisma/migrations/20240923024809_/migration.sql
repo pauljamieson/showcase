a6840944cb0bf3f49cfd7d730ef499e350cfe6d5
Paul <paul.jamieson.ca@gmail.com>
2024-09-22 21:48:40 -0500

    update

---
+++ api/prisma/migrations/20240923024809_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rating` on the `VideoFile` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "VideoFile" DROP COLUMN "rating";
